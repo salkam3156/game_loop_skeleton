e1f28de64b6f589828ac9a515370433dbeab69b9
salkam3156 <salkam3156@hotmail.com>
2019-10-13 20:35:36 +0200

    added a todo

--- Systems/MouseHoverDetector.cs
+++ Systems/MouseHoverDetector.cs
@@ -6,6 +6,7 @@ namespace game_loop_skeleton.Systems
     public class MouseHoverDetector
     {
         //TODO: A POC; this will be inefficient for now
+        //TODO: check behaviour on scaled screen dims - the values for texture dimensions might not scale to the skew applied
         public bool IsHoveringOver(IGameObject gameObject)
         {
             var mousePos = Mouse.GetPosition();

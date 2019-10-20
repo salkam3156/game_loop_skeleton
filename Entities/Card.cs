793757401e632e495fc539f90a69856e81a8a6c3
salkam3156 <salkam3156@hotmail.com>
2019-10-20 18:14:50 +0200

    reoving whitespaces

--- Entities/Card.cs
+++ Entities/Card.cs
@@ -24,8 +24,6 @@ namespace game_loop_skeleton.Entities
         public void Move(Vector2f position)
         {
             Sprite.Position = new Vector2f(position.X - Sprite.TextureRect.Width / 2, position.Y - Sprite.TextureRect.Height / 2);
-
-
         }
 
     }

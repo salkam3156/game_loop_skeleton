682c31cfd55543ae3f11ba6f37da5551e87c6402
salkam3156 <salkam3156@hotmail.com>
2019-10-20 20:19:12 +0200

    framerate regulation

--- Main.cs
+++ Main.cs
@@ -4,6 +4,7 @@ using game_loop_skeleton.Utils;
 using game_loop_skeleton.Entities;
 using SFML.Window;
 using game_loop_skeleton.Systems;
+using SFML.System;
 
 namespace SFML_Lechu
 {
@@ -15,6 +16,10 @@ namespace SFML_Lechu
             using (var musicPlayer = new MusicPlayer())
             {
                 //Scribbles/debug
+                var clock = new Clock();
+                var framerPerSecond = 60;
+                var frameTime = Time.FromMilliseconds(1000 / framerPerSecond);
+
                 game.RenderTarget.Closed += (o, e) => { game.IsRunning = false; };
                 var textureLoader = new TextureLoader();
                 var textureFlyweight = new TextureFlyweight(@"res/ace.png", textureLoader);
@@ -30,6 +35,7 @@ namespace SFML_Lechu
                 //Loop
                 while (game.IsRunning)
                 {
+
                     game.RenderTarget.DispatchEvents();
 
                     //Handle input
@@ -43,15 +49,21 @@ namespace SFML_Lechu
                     commandToExecute?.ExecuteOn(testEntCards[0]);
 
                     //Draw
-                    renderTarget.Clear();
-                    renderTarget.Draw(bg.Sprite);
-
-                    foreach (var card in testEntCards)
+                    if (clock.ElapsedTime >= frameTime)
                     {
-                        renderTarget.Draw(card.Sprite);
+                        clock.Restart();
+
+                        renderTarget.Clear();
+                        renderTarget.Draw(bg.Sprite);
+
+                        foreach (var card in testEntCards)
+                        {
+                            renderTarget.Draw(card.Sprite);
+                        }
+
+                        renderTarget.Display();
                     }
 
-                    renderTarget.Display();
 
                     //Update state
                 }
